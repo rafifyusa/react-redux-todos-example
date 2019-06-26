6e38050fd0cc3626883bad8b8feaad3247e5b610
Rafif Reyhandhia Yusa <Rafif_RY276@mitrais.com>
2019-06-26 16:17:23 +0800

    Fetching data on Route change

---
+++ src/api/index.js
@@ -0,0 +1,37 @@
+import { v4 } from "node-uuid";
+
+const fakeDatabase = {
+  todos: [
+    {
+      id: v4(),
+      text: "1st task",
+      completed: true
+    },
+    {
+      id: v4(),
+      text: "2nd task",
+      completed: true
+    },
+    {
+      id: v4(),
+      text: "3rd task",
+      completed: false
+    }
+  ]
+};
+
+const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+export const fetchTodos = filter =>
+  delay(500).then(() => {
+    switch (filter) {
+      case "all":
+        return fakeDatabase.todos;
+      case "active":
+        return fakeDatabase.todos.filter(t => !t.completed);
+      case "completed":
+        return fakeDatabase.todos.filter(t => t.completed);
+      default:
+        throw new Error(`Unknown filter: ${filter}`);
+    }
+  });
