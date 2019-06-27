8eb8e31f8df9db9d3f5a7b1ca47d0f181653d120
Rafif Reyhandhia Yusa <Rafif_RY276@mitrais.com>
2019-06-27 13:39:16 +0800

    Dispatching Actions Asynchronously with Thunks

--- src/store/store.js
+++ src/store/store.js
@@ -1,32 +1,14 @@
 import { createStore, applyMiddleware } from "redux";
-import promise from "redux-promise";
+
 import { createLogger } from "redux-logger";
 
 import todoApp from "../reducers/index";
 
-// const logger = store => next => {
-//   if (!console.group) {
-//     return next;
-//   }
-//   return action => {
-//     console.group(action.type);
-//     console.log("%c prev state", "color:gray", store.getState());
-//     console.log("%c action", "color: blue", action);
-//     const returnValue = next(action);
-//     console.log("%c next state", "color: green", store.getState());
-//     console.groupEnd(action.type);
-//   };
-// };
-
-// const promiseMiddleware = store => next => action => {
-//   if (typeof action.then === "function") {
-//     return action.then(next);
-//   }
-//   return next(action);
-// };
+const thunk = store => next => action =>
+  typeof action === "function" ? action(store.dispatch) : next(action);
 
 const configureStore = () => {
-  const middlewares = [promise];
+  const middlewares = [thunk];
 
   if (process.env.NODE_ENV !== "production") {
     middlewares.push(createLogger());
