0c02db3db5079476c2b99e19924d5f6ad7f25fa8
Rafif Reyhandhia Yusa <Rafif_RY276@mitrais.com>
2019-06-27 09:37:52 +0800

    Middleware Chaine

--- src/store/store.js
+++ src/store/store.js
@@ -11,38 +11,46 @@ import todoApp from "../reducers/index";
 //   ]
 // };
 
-const addLoggingToDispatch = store => {
-  const rawDispatch = store.dispatch;
-
+const logger = store => next => {
   if (!console.group) {
-    return rawDispatch;
+    return next;
   }
   return action => {
     console.group(action.type);
     console.log("%c prev state", "color:gray", store.getState());
     console.log("%c action", "color: blue", action);
-    const returnValue = rawDispatch(action);
+    const returnValue = next(action);
     console.log("%c next state", "color: green", store.getState());
     console.groupEnd(action.type);
   };
 };
 
-const addPromiseSupportToDispatch = store => {
-  const rawDispatch = store.dispatch;
-  return action => {
-    if (typeof action.then === "function") {
-      return action.then(rawDispatch);
-    }
-    return rawDispatch(action);
-  };
+const promiseMiddleware = store => next => action => {
+  if (typeof action.then === "function") {
+    return action.then(next);
+  }
+  return next(action);
 };
+
+const wrapDispatchWithMiddlewares = (store, middlewares) => {
+  middlewares
+    .slice()
+    .reverse()
+    .forEach(middleware => {
+      store.dispatch = middleware(store)(store.dispatch);
+    });
+};
+
 const configureStore = () => {
   const store = createStore(todoApp);
 
+  const middlewares = [promiseMiddleware];
+
   if (process.env.NODE_ENV !== "production") {
-    store.dispatch = addLoggingToDispatch(store);
+    middlewares.push(logger);
   }
-  store.dispatch = addPromiseSupportToDispatch(store);
+
+  wrapDispatchWithMiddlewares(store, middlewares);
   return store;
 };
 
