e274c0e9c163ccf0f4d63e8a9f02ca6fbe1001ee
Rafif Reyhandhia Yusa <Rafif_RY276@mitrais.com>
2019-06-27 11:23:35 +0800

    Refactoring the Reducers

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import { v4 } from "node-uuid";
 import * as api from "../api";
 
-export const receiveTodos = (filter, response) => ({
+const receiveTodos = (filter, response) => ({
   type: "RECEIVE_TODOS",
   filter,
   response
@@ -9,6 +9,7 @@ export const receiveTodos = (filter, response) => ({
 
 export const fetchTodos = filter =>
   api.fetchTodos(filter).then(response => receiveTodos(filter, response));
+
 export const addTodo = text => ({
   type: "ADD_TODO",
   id: v4(),
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Todo from "./Todo";
+
 const TodoList = ({ todos, onTodoClick }) => (
   <ul>
     {todos.map(todo => (
